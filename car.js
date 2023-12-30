330fff704b841378934d6117aafe9ea05ce33e2b
benshabbat <benshabbat27@gmail.com>
2023-12-30 21:49:39 +0200

    fix syntax

--- car.js
+++ car.js
@@ -1,15 +1,18 @@
-class Car{
-    constructor(x, y, width, height){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
+class Car {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
 
-    draw(ctx){
-        ctx.beginPath();
-        ctx.rect(
-            this.x-this.width/2,this.y-this.height/2,this.width,this.height);
-        )
-    }
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(
+      this.x - this.width / 2,
+      this.y - this.height / 2,
+      this.width,
+      this.height
+    );
+  }
 }
