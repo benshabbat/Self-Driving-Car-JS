e79091efe3dae9ba38a37f474c0c8242093b71ff
benshabbat <benshabbat27@gmail.com>
2024-01-22 11:52:54 +0200

    onkeydown onkeyup

--- controls.js
+++ controls.js
@@ -9,7 +9,24 @@ class Controls {
   }
   #addKeyboardListeners() {
     document.onkeydown = (e) => {
-      console.log(e);
+      switch (e.key)
+      {
+        case 'ArrowUp':this.forward=true; break;
+        case 'ArrowDown':this.reverse=true; break;
+        case 'ArrowLeft':this.left=true; break;
+        case 'ArrowRight':this.right=true; break;
+      }
+
+    };
+    document.onkeyup = (e) => {
+      switch (e.key)
+      {
+        case 'ArrowUp':this.forward=false; break;
+        case 'ArrowDown':this.reverse=false; break;
+        case 'ArrowLeft':this.left=false; break;
+        case 'ArrowRight':this.right=false; break;
+      }
+
     };
   }
 }
