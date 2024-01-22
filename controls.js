d66645cfe1b17197fbe49096b3b9b425b5a41780
benshabbat <benshabbat27@gmail.com>
2024-01-22 12:00:08 +0200

    debugging

--- controls.js
+++ controls.js
@@ -9,24 +9,39 @@ class Controls {
   }
   #addKeyboardListeners() {
     document.onkeydown = (e) => {
-      switch (e.key)
-      {
-        case 'ArrowUp':this.forward=true; break;
-        case 'ArrowDown':this.reverse=true; break;
-        case 'ArrowLeft':this.left=true; break;
-        case 'ArrowRight':this.right=true; break;
+      switch (e.key) {
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowDown":
+          this.reverse = true;
+          break;
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+          break;
       }
-
+      console.table(this);
     };
     document.onkeyup = (e) => {
-      switch (e.key)
-      {
-        case 'ArrowUp':this.forward=false; break;
-        case 'ArrowDown':this.reverse=false; break;
-        case 'ArrowLeft':this.left=false; break;
-        case 'ArrowRight':this.right=false; break;
+      switch (e.key) {
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
       }
 
+      console.table(this);
     };
   }
 }
